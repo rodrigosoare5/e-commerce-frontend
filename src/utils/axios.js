31873657b077f107dfbf75a315708d3eb4e0cd7f
rodrigo <rodrigo@gmail.com>
2022-12-01 02:46:04 -0300

    ajustes nas funcionalidades

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL =   "http://127.0.0.1:8000/api/" 
+const baseURL =   "https://ecommerce-api-rodrigo.herokuapp.com/api/" 
 
 const useAxios = axios.create({
 	baseURL: baseURL,
